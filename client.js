ebcbc0d8b9e5ef88078cb62d49767a6324b68dcc
Robbie Prokop <robbieprokop@gmail.com>
2022-04-14 01:43:53 +0000

    added key strokes to move

--- client.js
+++ client.js
@@ -10,15 +10,12 @@ const connect = function (){
   conn.on("connect", (data)=> {
     //code that does something once the connection is established
     console.log("Be happy. Now move your snake.")
-    conn.write('Name: ROL')
-    
+    conn.write('Name: RJP') 
     // for (let i = 0; i< 10; i++){
     //   setTimeout(()=>{
     //     conn.write('Move: up')
     //   },1000*i)
-
     // }
-
     //times must be different or the server will get confused and pick the last one
     // setTimeout(()=>{
     //   conn.write('Move: left')
@@ -29,27 +26,33 @@ const connect = function (){
     // setTimeout(()=>{
     //   conn.write('Move: left')
     // },400)
-    // setTimeout(()=>{
-    //   conn.write('Move: left')
-    // },500)
-    // setTimeout(()=>{
-    //   conn.write('Move: left')
-    // },600)
-    // setTimeout(()=>{
-    //   conn.write('Move: left')
-    // },700)
-  
+
   })
-  conn.on("connect", (data)=> {
+  // conn.on("connect", (data)=> {
     //code that does something once the connection is established
-    setTimeout(()=>{
-      conn.write('Move: left')
-    },200)
-  
-  })
+    //this is hardcoded
+    // setTimeout(()=>{
+    //   conn.write('Move: left')
+    // },200)
+  // })
   conn.setEncoding("utf8")
+  conn.on("end", ()=> {
+    process.exit()
+  }) 
   return conn
 }
+
+  // if(key === "a"){
+  //  conn.write('Move: left') 
+  // }
+  // if(key === "s"){
+  //  conn.write('Move: down') 
+  // }
+  // if(key === "d"){
+  //  conn.write('Move: right') 
+  // }
+
+
 
 
 module.exports = connect
--- play.js
+++ play.js
@@ -1,4 +1,4 @@
-const net = require('net');
+
 const connect = require("./client")
 
 // establishes a connection with the game server
@@ -13,6 +13,36 @@ const connect = require("./client")
 
 //   return conn;
 // };
+const conn = connect();
+
+const handleUserInput = function(key){
+  console.log(key)
+  if( key === '\u0003'){
+    process.exit();
+  }
+  if( key === 'w'){
+    conn.write('Move: up')
+  }
+  if( key === 'a'){
+    conn.write('Move: left')
+  }
+  if( key === 's'){
+    conn.write('Move: down')
+  }
+  if( key === 'd'){
+    conn.write('Move: right')
+  }
+
+}
+const setupInput = function (conn) {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on('data', handleUserInput)
+  return stdin;
+};
+
 
 console.log("Connecting ...");
-connect();
+setupInput(conn);
